00a9f45b8e1dc48c70ec246697ad46aba89e05cd
jidni <gidni441@gmail.com>
2024-12-20 14:53:06 +0600

    log in log out works perfected

---
+++ models/teken.ts
@@ -0,0 +1,5 @@
+export interface token {
+    id: string,
+    mail:string,
+    pass:string
+}
---
+++ src/app/service/token.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { token } from 'models/teken';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+
+
+
+  private path = "http://localhost:3000/tokens/";
+  // private path = "http://192.168.20.135:3000/users/";
+
+  constructor(private http: HttpClient) { }
+
+  // Get all users
+  getAll() {
+    return this.http.get<token[]>(this.path);
+  }
+
+  // Get user by ID
+  getById(id: string) {
+    return this.http.get(`${this.path}${id}`);
+  }
+
+  // Add a new user
+  add(token: any) {
+    return this.http.post(this.path, token);
+  }
+
+  // Update user by ID
+  update(id: string, token: any) {
+    return this.http.put(`${this.path}${id}`, token);
+  }
+
+  // Delete user by ID
+  delete(id: string) {
+    return this.http.delete(`${this.path}${id}`);
+  }
+}
